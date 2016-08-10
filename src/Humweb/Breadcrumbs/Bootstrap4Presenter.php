1b17cd3265e11ac59a552a4853c8a994607973e5
Ryan Shofner <ryan@letsgolearn.com>
2016-08-09 23:25:14 -0700

    extend presenter

--- src/Humweb/Breadcrumbs/Bootstrap4Presenter.php
+++ src/Humweb/Breadcrumbs/Bootstrap4Presenter.php
@@ -1,43 +1,6 @@
 <?php namespace Humweb\Breadcrumbs;
 
-class Bootstrap4Presenter {
-
-	protected $breadcrumb;
-
-	public function __construct(Breadcrumbs $breadcrumb)
-	{
-		$this->breadcrumb = $breadcrumb;
-	}
-
-	/**
-	 * Render the Bootstrap breadcrumbs contents.
-	 *
-	 * @return string
-	 */
-	public function render()
-	{
-		$pages = array();
-		$crumbs = $this->breadcrumb->toArray();
-
-		foreach ($crumbs as $item)
-		{
-
-			if ( empty($item['url']) )
-			{
-				$pages[] = $this->getActiveLink($item);
-			}
-			else
-			{
-				$pages[] = $this->getLink($item);
-			}
-		}
-		return $this->getWrapper(implode('', $pages));
-	}
-
-	public function getWrapper($items)
-	{
-		return '<ol class="breadcrumb">'.$items.'</ol>';
-	}
+class Bootstrap4Presenter extends Presenter {
 
 	public function getLink($item)
 	{
